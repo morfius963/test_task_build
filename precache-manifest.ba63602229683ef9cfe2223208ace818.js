f40becf1c66723fbc7f74ec2b552aa664ac5c1eb
Vitalii <39135104+morfius963@users.noreply.github.com>
2020-05-20 02:12:39 +0300

    Add files via upload

---
+++ precache-manifest.ba63602229683ef9cfe2223208ace818.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9281e3226a5fb8c84f7c330dbbcea745",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dc12fc40f8f144f1a02c",
+    "url": "./static/css/main.32c712b7.chunk.css"
+  },
+  {
+    "revision": "8d05da11d119625da82d",
+    "url": "./static/js/2.9d5e0f80.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.9d5e0f80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc12fc40f8f144f1a02c",
+    "url": "./static/js/main.58e828c7.chunk.js"
+  },
+  {
+    "revision": "f2781cb458e63910de33",
+    "url": "./static/js/runtime-main.38d8431c.js"
+  }
+]);
